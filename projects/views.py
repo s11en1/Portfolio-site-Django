5423e6c4ee8b1abd1d055f10d3f7f01cc103cde1
s11en1 <sunset.savorer@gmail.com>
2024-05-10 14:14:32 +0300

    Add index view in projects app

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from projects.models import Project
 
-# Create your views here.
+def project_index(request):
+    template = 'project_index.html'
+    projects = Project.objects.all()
+    context = {
+        'projects': projects,
+    }
+    return render(request, template, context)
+
+def project_detail(request, project_id):
+    template = 'project_detailed.html'
+    project = Project.objects.get(pk=project_id)
+    context = {
+        'project': project,
+    }
+    return render(request, template, context)
